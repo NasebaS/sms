28b12b68067ff76e2ce78e7d2dbaa61c24e29f89
Naseba Sathurudeen <naseba.hamee@gmail.com>
2023-07-12 22:33:21 -0700

    added

--- QRCodeScreen.js
+++ QRCodeScreen.js
@@ -1,364 +1,7 @@
-// import React, { useState, useEffect } from 'react';
-// import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
-// import { RNCamera } from 'react-native-camera';
-// import QRCodeScanner from 'react-native-qrcode-scanner';
-// import { useNavigation } from '@react-navigation/native';
 
-// const QRCodeScreen = () => {
-//   const [scanning, setScanning] = useState(false);
-//   const [barcodeData, setBarcodeData] = useState(null);
-//   const details = {
-//     productName: 'SMS D/W FANCY CHTRN BUTTA CATALOGUE DESIGN',
-//     smsCode: 'SMSMXDG23',
-//     wholesalePrice: 'Rs. 52000',
-//     retailPrice: 'Rs. 52000',
-//   };
-//   const navigation = useNavigation();
-//   useEffect(() => {
-//     startScanning();
-//   }, []);
-
-//   const startScanning = () => {
-//     setScanning(true);
-//   };
-
-//   // const handleBarCodeRead = async (event) => {
-//   //   if (!scanning) {
-//   //     return;
-//   //   }
-//   //   setScanning(false);
-
-//   //   // Access the scanned data from event.data
-//   //   const scannedData = event.data;
-//   //   Alert.alert(scannedData)
-//   //   setBarcodeData(scannedData);
-   
-//   //   // Fetch the details from the URL
-//   //   // try {
-//   //   //   const response = await fetch(scannedData);
-//   //   //   const data = await response.json();
-//   //   //   setDetails(data);
-     
-//   //   // } catch (error) {
-//   //   //   Alert.alert('Error', 'Invalid BarCode');
-//   //   // }
-//   // };
-// const handleBarCodeRead = (event) => {
-//     if (!scanning) {
-//       return;
-//     }
-
-//     // Access the scanned data from event.data
-//     const scannedData = event.data;
-
-//     // Show an alert with the scanned data
-//     Alert.alert('Scanned QR Code', scannedData);
-
-//     // Resume scanning
-//     setScanning(true);
-//   };
-//   return (
-//     <View style={styles.container}>
-//       <QRCodeScanner
-//         reactivate={true}
-//         showMarker={true}
-//         onRead={handleBarCodeRead}
-//         cameraProps={{ captureAudio: false }}
-//         cameraContainerStyle={styles.cameraContainer}
-//         cameraStyle={styles.camera}
-//       />
-//       {details && (
-//         <View style={styles.detailsContainer}>
-//           <Text style={styles.detailsText}>
-//             <Text style={styles.productNameText}>Product Name: </Text>
-//   {details.productName}
-// </Text>
-// <Text style={styles.detailsText}>
-// <Text style={styles.smsCodeText}>
-//   SMS Code: </Text>
-//   {details.smsCode}
-// </Text>
-// <Text style={styles.detailsText}>
-//   <Text style={styles.retailPriceText}>
-//   Retail Price: 
-//   </Text>
-//   <Text style={styles.boldText}>{details.retailPrice}</Text>
-// </Text>
-// <Text style={styles.detailsText}>
-//   <Text style={styles.wholesalePriceText}>
-//   Wholesale Price: 
-//   </Text>
-//   <Text style={styles.boldText}>{details.wholesalePrice}</Text>
-// </Text>
-
-//         </View>
-//       )}
-//       <View style={styles.bottomView}>
-      
-//       <TouchableOpacity style={styles.backButton} onPress={()=>navigation.navigate('Scan')}>
-//         <Text style={styles.backButtonText}>Go Back</Text>
-//       </TouchableOpacity>
-//       </View>
-      
-//     </View>
-//   );
-// };
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   cameraContainer: {
-//         position: 'absolute',
-//         top: 0,
-//         left: 0,
-//         width: '100%',
-//         height: '100%',
-//         borderWidth: 1,
-//         borderColor: 'white',
-//       },
-//   camera: {
-//     flex: 1,
-//     width: '100%',
-//   },
-//   detailsContainer: {
-//     position: 'absolute',
-//     top:420,
-//        left: 0,
-//     right: 0,
-//     backgroundColor: 'white',
-//     padding: 20,
-//   },
-//   detailsText: {
-//     color: 'black',
-//     fontSize: 16,
-//     marginBottom: 10,
-//   },
-//   productNameText: {
-//     color: '#5A96E3',
-//   },
-//   smsCodeText: {
-//     color: '#5A96E3',
-//   },
-//   retailPriceText: {
-//     color: '#5A96E3',
-//   },
-//   wholesalePriceText: {
-//     color: '#5A96E3',
-//   },
-
-//   button: {
-//     backgroundColor: '#2196F3',
-//     paddingVertical: 10,
-//     paddingHorizontal: 20,
-//     borderRadius: 5,
-//     marginTop: 20,
-//     marginBottom:30,
-//   },
-//   buttonText: {
-//     color: 'white',
-//     fontWeight: 'bold',
-//     fontSize: 16,
-//   },
-//   boldText:{
-// fontWeight:'bold'
-//   },
-//   bottomView:{
-//     flexDirection:'row',
-//     justifyContent: 'space-between',
-//     marginRight: 20,
-    
-//   },
-//   backButton:{
-//     backgroundColor: '#A8A196',
-//     paddingVertical: 10,
-//     paddingHorizontal: 20,
-//     borderRadius: 20,
-//     marginTop: 20,
-//     marginBottom:30,
-//     marginLeft:25,
-//   },
-//   backButtonText: {
-//     color: 'black',
-//     fontWeight: 'bold',
-//     fontSize: 16,
-//   },
-// });
-
-// export default QRCodeScreen;
-
-
-// import React, { useState, useEffect } from 'react';
-// import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
-// import { RNCamera } from 'react-native-camera';
-// import QRCodeScanner from 'react-native-qrcode-scanner';
-// import { useNavigation } from '@react-navigation/native';
-
-// const QRCodeScreen = () => {
-//   const [scanning, setScanning] = useState(false);
-//   const [barcodeData, setBarcodeData] = useState(null);
-//   const details = {
-//     productName: 'SMS D/W FANCY CHTRN BUTTA CATALOGUE DESIGN',
-//     smsCode: 'SMSMXDG23',
-//     wholesalePrice: 'Rs. 52000',
-//     retailPrice: 'Rs. 52000',
-//   };
-//   const navigation = useNavigation();
-//   useEffect(() => {
-//     startScanning();
-//   }, []);
-
-//   const startScanning = () => {
-//     setScanning(true);
-//   };
-
-//   const handleBarCodeRead = (event) => {
-//     if (!scanning) {
-//       return;
-//     }
-//     const scannedData = event.data;
-//     Alert.alert('Scanned QR Code', scannedData, [
-//       {
-//         text: 'OK',
-//         onPress: () => {
-//           setTimeout(() => {
-//             setScanning(true);
-//           }, 2000);
-//         },
-//       },
-//     ]);
-//     setScanning(false);
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <QRCodeScanner
-      
-//         reactivate={true}
-//         showMarker={true}
-//         onRead={handleBarCodeRead}
-//         cameraProps={{ captureAudio: false }}
-//         cameraContainerStyle={styles.cameraContainer}
-//         cameraStyle={styles.camera}
-//         topViewStyle={styles.topView}
-//         bottomViewStyle={styles.bottomView}
-//         markerStyle={styles.marker}
-//         vibrate={false}
-//       />
-//       {details && (
-//         <View style={styles.detailsContainer}>
-//           <Text style={styles.detailsText}>
-//             <Text style={styles.productNameText}>Product Name: </Text>
-//             {details.productName}
-//           </Text>
-//           <Text style={styles.detailsText}>
-//             <Text style={styles.smsCodeText}>SMS Code: </Text>
-//             {details.smsCode}
-//           </Text>
-//           <Text style={styles.detailsText}>
-//             <Text style={styles.retailPriceText}>Retail Price: </Text>
-//             <Text style={styles.boldText}>{details.retailPrice}</Text>
-//           </Text>
-//           <Text style={styles.detailsText}>
-//             <Text style={styles.wholesalePriceText}>Wholesale Price: </Text>
-//             <Text style={styles.boldText}>{details.wholesalePrice}</Text>
-//           </Text>
-//         </View>
-//       )}
-//       <View style={styles.bottomView}>
-//         <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('Scan')}>
-//           <Text style={styles.backButtonText}>Go Back</Text>
-//         </TouchableOpacity>
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   camera: {
-//     flex: 1,
-//     width: '100%',
-//   },
-//   topView: {
-//     flex: 0,
-//     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-//     padding: 16,
-//     paddingTop: 40,
-//     paddingBottom: 40,
-//     justifyContent: 'flex-start',
-//     alignItems: 'center',
-//   },
-//   bottomView: {
-//     flex: 0,
-//     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-//     padding: 16,
-//     paddingTop: 40,
-//     paddingBottom: 40,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   marker: {
-//     borderWidth: 2,
-//     borderColor: 'white',
-//   },
-//   detailsContainer: {
-//     position: 'absolute',
-//     bottom: 100,
-//     left: 20,
-//     right: 20,
-//     backgroundColor: 'white',
-//     padding: 20,
-//     borderRadius: 10,
-//   },
-//   detailsText: {
-//     color: 'black',
-//     fontSize: 16,
-//     marginBottom: 10,
-//   },
-//   productNameText: {
-//     color: '#5A96E3',
-//     fontWeight: 'bold',
-//   },
-//   smsCodeText: {
-//     color: '#5A96E3',
-//     fontWeight: 'bold',
-//   },
-//   retailPriceText: {
-//     color: '#5A96E3',
-//     fontWeight: 'bold',
-//   },
-//   wholesalePriceText: {
-//     color: '#5A96E3',
-//     fontWeight: 'bold',
-//   },
-//   backButton: {
-//     position: 'absolute',
-//     bottom: 30,
-//     backgroundColor: '#A8A196',
-//     paddingVertical: 10,
-//     paddingHorizontal: 20,
-//     borderRadius: 20,
-//   },
-//   backButtonText: {
-//     color: 'black',
-//     fontWeight: 'bold',
-//     fontSize: 16,
-//   },
-// });
-
-
-// export default QRCodeScreen;
 
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity,Animated , TouchableHighlight, Alert } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { useNavigation } from '@react-navigation/native';
@@ -373,6 +16,8 @@ const QRCodeScreen = () => {
     retailPrice: 'Rs. 52000',
   };
   const navigation = useNavigation();
+  const [buttonScale] = useState(new Animated.Value(1));
+
   useEffect(() => {
     startScanning();
   }, []);
@@ -397,6 +42,25 @@ const QRCodeScreen = () => {
       },
     ]);
     setScanning(false);
+  };
+  const goBack = () => {
+    navigation.navigate('Scan');
+  };
+  const handleButtonPress = () => {
+    Animated.sequence([
+      Animated.timing(buttonScale, {
+        toValue: 1.2,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(buttonScale, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      goBack();
+    });
   };
   // function marker(color: string, size: string | number, borderLength: string | number, thickness: number = 2, borderRadius: number = 0): JSX.Element {
   //   return <View style={{ height: size, width: size }}>
@@ -447,9 +111,14 @@ const QRCodeScreen = () => {
         </View>
       )}
       <View style={styles.bottomView}>
-        <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('Scan')}>
-          <Text style={styles.backButtonText}>Go Back</Text>
-        </TouchableOpacity>
+      <Animated.View style={[styles.backButton, { transform: [{ scale: buttonScale }] }]}>
+          <TouchableOpacity
+            onPress={handleButtonPress}
+            activeOpacity={0.7}
+          >
+            <Text style={styles.backButtonText}>Go Back</Text>
+          </TouchableOpacity>
+        </Animated.View>
       </View>
     </View>
   );
@@ -539,11 +208,18 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 20,
+    marginTop: 20,
+    transform: [{ translateY: -10 }],
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 5,
   },
   backButtonText: {
     color: 'black',
     fontWeight: 'bold',
-    fontSize: 15,
+    fontSize: 14,
   },
   wrapper: {
     flexDirection: 'row',
